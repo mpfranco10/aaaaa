254235558579c3bf0e461dcdf07c40a788a451c4
María Paula <30930610+mpfranco10@users.noreply.github.com>
2018-09-27 15:26:49 -0500

    Add files via upload

---
+++ consumer-producer.py
@@ -0,0 +1,21 @@
+import json, time
+from kafka import KafkaConsumer, KafkaProducer
+from kafka.errors import KafkaError
+from random import uniform
+
+consumer = KafkaConsumer(bootstrap_servers=['172.24.41.165:8081'],
+                         value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+consumer.subscribe(pattern='.*.*.*')
+
+producer = KafkaProducer(bootstrap_servers=['172.24.41.165:8081'], 
+             value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+for message in consumer:
+    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
+
+
+
+
+
